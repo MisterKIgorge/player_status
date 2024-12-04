b75180563c95729c1b5367a5f8ae5e749d73e7e9
gibberish <gbjgms@gmail.com>
2024-12-04 18:57:57 +0700

    Removed InitEventListeners

--- scripts/components/player_status.lua
+++ scripts/components/player_status.lua
@@ -4,32 +4,32 @@ local PlayerInformation = nil
 PlayerInformation = Class(function(self, inst)
     self.inst = inst
 
-    self:InitEventListeners()
+    -- self:InitEventListeners()
 end)
 
-function PlayerInformation:InitEventListeners()
-    for i, data in pairs(StatusDefs.GetStatusData()) do
-        for i, event in pairs(data.events) do
-            local function UpdateServerData()
-                local remote_data = {
-                    stats = data.fn(self.inst),
-                    name = data.tooltip,
-                    owner = self.inst,
-                }
+-- function PlayerInformation:InitEventListeners()
+--     for i, data in pairs(StatusDefs.GetStatusData()) do
+--         for i, event in pairs(data.events) do
+--             local function UpdateServerData()
+--                 local remote_data = {
+--                     stats = data.fn(self.inst),
+--                     name = data.tooltip,
+--                     owner = self.inst,
+--                 }
 
-                for i, player in ipairs(AllPlayers) do
-                    if self.inst ~= player then
-                        TheNetEvent:PushEventOnOwnerEntity(player.GUID, self.inst.GUID, "net_" .. event, remote_data)
-                    else
-                        self.inst:PushEvent("net_" .. event, remote_data)
-                    end
-                end
-            end
+--                 for i, player in ipairs(AllPlayers) do
+--                     if self.inst ~= player then
+--                         TheNetEvent:PushEventOnOwnerEntity(player.GUID, self.inst.GUID, "net_" .. event, remote_data)
+--                     else
+--                         self.inst:PushEvent("net_" .. event, remote_data)
+--                     end
+--                 end
+--             end
 
-            self.inst:RemoveEventCallback(event, UpdateServerData)
-            self.inst:ListenForEvent(event, UpdateServerData)
-        end
-    end
-end
+--             self.inst:RemoveEventCallback(event, UpdateServerData)
+--             self.inst:ListenForEvent(event, UpdateServerData)
+--         end
+--     end
+-- end
 
 return PlayerInformation
